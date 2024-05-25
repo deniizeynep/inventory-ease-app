49e7cae98002fdfcfb84b86fffb61db28995590f
Zeynep Deniz <dnzzeynep7@gmail.com>
2024-05-25 12:48:17 +0300

    Draft application has been created

---
+++ app/(tabs)/Inventory.tsx
@@ -0,0 +1,76 @@
+import {
+    View,
+    TextInput,
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    ScrollView,
+  } from 'react-native';
+  import { FontAwesome } from '@expo/vector-icons';
+  
+  export default function Inventory() {
+    return (
+      <ScrollView style={{backgroundColor: '#ffff'}}>
+        <View style={styles.container}>
+          <Text style={styles.productName}>aa</Text>
+        </View>
+        <View style={styles.containerr}>
+          <Text style={styles.productName}>aa</Text>
+        </View>
+        <View style={styles.containerr}>
+          <Text style={styles.productName}>aa</Text>
+        </View>
+        <View style={styles.containerr}>
+          <Text style={styles.productName}>aa</Text>
+        </View>
+  
+        <View style={styles.trans}>
+          <Text style={styles.who}>aa</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      width: '90%',
+      height: 70,
+      marginTop: 40,
+      marginLeft: 20,
+      backgroundColor: '#e3e3e3',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    },
+    containerr: {
+      width: '90%',
+      height: 70,
+      marginTop: 20,
+      marginLeft: 20,
+      backgroundColor: '#e3e3e3',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    },
+    productName: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    trans: {
+      width: '90%',
+      height: 70,
+      marginTop: 20,
+      marginLeft: 20,
+      backgroundColor: '#e3e3e3',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    },
+    who: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  });
+  
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,72 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import AddItems from './AddItems';
+import Cart from './Cart';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import Inventory from './Inventory';
+import StockCalendar from './StockCalendar';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const Tab = createBottomTabNavigator();
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function Layout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#345832',
+          inactiveTintColor: '#363636',
+          showLabel: false,
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#345832',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 25,
+          },
+          headerTitleAlign: 'center',
+        }}>
+        <Tab.Screen
+          name="Cart"
+          component={Cart}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="payment" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add Items"
+          component={AddItems}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="shopping-basket" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Inventory"
+          component={Inventory}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="inventory" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Stock Calendar"
+          component={StockCalendar}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
